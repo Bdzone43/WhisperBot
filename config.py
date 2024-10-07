6aadb77323962576fe6d0bec47a0ee2a4c830431
Bdzone43 <150906861+Bdzone43@users.noreply.github.com>
2024-10-07 21:49:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 # Channel: https://t.me/itsSmartDev
 
 # Pyrogram setup
-API_ID = "123456"  # Replace this API ID with your actual API ID
-API_HASH = "12123456ABCDEFGHI  # Replace this API HASH with your actual API HASH
-BOT_TOKEN = "12345678:XXXXXXXXXXXXXXXX"  # Replace this BOT_TOKEN
+API_ID = "23893446"  # Replace this API ID with your actual API ID
+API_HASH = "6334c14b3333f0ad2506d81574c54d40  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "7884933134:AAFY9jknc1loddy37X-ILyB2guReKphI66g"  # Replace this BOT_TOKEN
 
 # Bot USER_NAME
 BOT_USERNAME = "LockTextBot" # Dont Use @ Just Put the bot username
